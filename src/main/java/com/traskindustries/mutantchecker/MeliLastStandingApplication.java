30961742af2c525310bf7ded4615790de893e513
Gilbert <gilbert.ar.gonzalez@gmail.com>
2018-11-12 22:35:25 -0300

    first commit

---
+++ src/main/java/com/traskindustries/mutantchecker/MeliLastStandingApplication.java
@@ -0,0 +1,12 @@
+package com.traskindustries.mutantchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliLastStandingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeliLastStandingApplication.class, args);
+	}
+}
