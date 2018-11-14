cf59e8f58cefa2e24d3175318fceacc8b81bec5e
GB <gb@iunigo.com>
2018-11-14 10:04:31 -0300

    API REST implementation

---
+++ src/test/java/com/traskindustries/dto/VerifyIsMutantRequest.java
@@ -0,0 +1,16 @@
+package com.traskindustries.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class VerifyIsMutantRequest {
+    private String[] dna;
+}
