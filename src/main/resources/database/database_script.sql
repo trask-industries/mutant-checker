bfdfb3f2163aa56821dd747c9192bde1977abd74
GB <gb@iunigo.com>
2018-11-14 20:01:44 -0300

    database script

---
+++ src/main/resources/database/database_script.sql
@@ -0,0 +1,15 @@
+create schema mutants;
+
+use mutants;
+
+create table checkeddna (
+  id bigint not null auto_increment, 
+  created datetime, 
+  dna longtext, 
+  hash varchar(40), 
+  result bit not null, 
+  primary key (id));
+  
+create index IX_result on checkeddna (result);
+
+alter table checkeddna add constraint UK_hash unique (hash);
